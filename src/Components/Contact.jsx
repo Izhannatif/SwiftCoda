432c3802bf9ebe3adc8fd15214ebeb277fe1ef10
Izhann <izhan124@gmail.com>
2024-06-28 16:06:31 +0500

    commit

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react'
 import Marquee from "react-fast-marquee";
-
+import { MdArrowOutward } from "react-icons/md";
 const Contact = () => {
     const [isContactInView, setIsContactInView] = useState(false);
     const contactRef = useRef(null);
@@ -30,14 +30,17 @@ const Contact = () => {
         <section id='contact' ref={contactRef} className={`py-10 transition-colors duration-700 ${isContactInView ? 'bg-white text-[#1f1f1f]' : 'bg-[#030303] text-white'}`}>
 
             <Marquee loop={0} speed={100} autoFill={true} className='overflow-hidden py-0 lg:py-2'>
-                <p className='text-6xl lg:text-8xl font-black uppercase tracking-tighter'> LET'S WORK TOGETHER.</p>
+                <p className='text-6xl lg:text-8xl font-black uppercase tracking-tighter'>LET'S WORK TOGETHER&nbsp;.&nbsp;</p>
             </Marquee>
             <Marquee loop={0} speed={100} autoFill={true} direction='right' className='overflow-hidden py-0 lg:py-2'>
-                <p className='text-6xl lg:text-8xl font-black uppercase tracking-tighter'> LET'S WORK TOGETHER.</p>
+                <p className='text-6xl lg:text-8xl font-black uppercase tracking-tighter'>LET'S WORK TOGETHER&nbsp;.&nbsp;</p>
             </Marquee>
 
             <div className='pl-1 lg:pl-20 pr-5 py-5 flex flex-col lg:flex-row justify-around items-center'>
-                <div className='text-6xl font-medium'>Have something in mind? 💡 <br /><span className='text-4xl'>Reach out to us.</span></div>
+                <div className='text-6xl font-medium'>Have something in mind? 💡 <br /><span className='pt-2 text-4xl flex'>Reach out to us.
+                    <a href='mailto:hello@swiftcoda.com'><MdArrowOutward size={40} className='ml-2 bg-[#1f1f1f] text-white rounded-full p-1 hover:translate-x-5 hover:rotate-45 transition-all duration-500' /></a>
+                </span>
+                </div>
                 <div className='text-3xl lg:text-5xl contact-email font-light animate-bounce pt-10 lg:p-0'>hello@swiftcoda.com</div>
             </div>
         </section>
