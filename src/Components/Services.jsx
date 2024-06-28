41fdf67f431461fa0019bbc087bb3ae5413bbd2c
Izhann <izhan124@gmail.com>
2024-06-28 10:28:05 +0500

    commit

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -136,7 +136,7 @@ const Services = () => {
                 setIsServicesInView(entry.isIntersecting);
             },
             {
-                threshold:0.5,
+                threshold: 0.5,
             }
         );
 
@@ -160,11 +160,11 @@ const Services = () => {
             >
                 <div className='flex justify-center items-center flex-col w-full'>
                     <div className='text-3xl font-semibold mb-5'>When it comes to</div>
-                    <div className='services-service text-4xl lg:text-7xl md:text-5xl w-full h-full flex flex-col justify-around items-center font-bold tracking-tighter'>
+                    <div className='services-service text-4xl lg:text-7xl md:text-5xl w-full h-full flex flex-col justify-around items-center font-bold tracking-tighter pb-10'>
                         {services.map((service, index) => (
-                            <Parallax key={service.name} translateX={index % 2 === 0 ? [25, -20] : [-25, 20]}>
+                            <Parallax key={service.name} translateX={index % 2 === 0 ? [50, -50] : [-50, 50]}>
                                 <div
-                                    className={`service-item px-5 py-2 mx-5 ${hoveredService && hoveredService.name !== service.name ? 'fade' : ''
+                                    className={`service-item px-5 py-2 mx-5 h-full ${hoveredService && hoveredService.name !== service.name ? 'fade' : ''
                                         }`}
                                     onMouseEnter={() => handleMouseEnter(service)}
                                     onMouseLeave={handleMouseLeave}
@@ -172,9 +172,9 @@ const Services = () => {
                                     <div>{service.name}</div>
 
                                     {hoveredService && hoveredService.name === service.name && (
-                                        <div className='categories'>
+                                        <div className='categories absolute flex flex-row flex-nowrap lg:flex-wrap justify-center top-full lg:top-1/2 left-0 lg:left-full text-nowrap pt-3 mb-10'>
                                             {hoveredService.categories.map((category, i) => (
-                                                <div key={i} className='category-item text-lg tracking-normal font-normal'>
+                                                <div key={i} className='category-item text-sm lg:text-lg tracking-tight font-normal bg-[#f5f5f5] text-[#1f1f1f] py-1 px-3 m-2 rounded-full'>
                                                     {category}
                                                 </div>
                                             ))}
@@ -184,14 +184,14 @@ const Services = () => {
                             </Parallax>
                         ))}
                     </div>
-                    {hoveredService && hoveredService.name === 'MARKETING' ? <div className='text-3xl font-semibold mb-5'>we are the storytellers.</div> : null}
-                    {hoveredService && hoveredService.name === 'BRANDING' ? <div className='text-3xl font-semibold mb-5'>we build legacies.</div> : null}
+                    {hoveredService && hoveredService.name === 'MARKETING' ? <div className='text-center text-xl lg:text-3xl font-semibold mb-5'>we are the storytellers.</div> : null}
+                    {hoveredService && hoveredService.name === 'BRANDING' ? <div className='text-center text-xl lg:text-3xl font-semibold mb-5'>we build legacies.</div> : null}
 
-                    {hoveredService && hoveredService.name === 'DESIGN' ? <div className='text-3xl font-semibold mb-5'>we speak the language of visuals.</div> : null}
+                    {hoveredService && hoveredService.name === 'DESIGN' ? <div className='text-center text-xl lg:text-3xl font-semibold mb-5'>we speak the language of visuals.</div> : null}
 
-                    {hoveredService && hoveredService.name === 'DEVELOPMENT' ? <div className='text-3xl font-semibold mb-5'> we bridge the gap between ideas and reality.</div> : null}
+                    {hoveredService && hoveredService.name === 'DEVELOPMENT' ? <div className='text-center text-xl lg:text-3xl font-semibold mb-5'> we bridge the gap between ideas and reality.</div> : null}
 
-                    {hoveredService && hoveredService.name === 'DIGITAL' ? <div className='text-3xl font-semibold mb-5'> we future proof your brand.</div> : null}
+                    {hoveredService && hoveredService.name === 'DIGITAL' ? <div className='text-center text-xl lg:text-3xl font-semibold mb-5'> we future proof your brand.</div> : null}
 
                     {!hoveredService && <div className='text-center text-3xl font-semibold mb-5'>we are your one stop solution</div>}
                 </div>
