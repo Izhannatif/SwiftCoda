79e3af6bdcdc472f71d9cb2102cbad6618679c15
Izhann <izhan124@gmail.com>
2024-07-04 03:41:42 +0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import Home from './Pages/Home'
 import { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
 import { CursorProvider } from './context/CursorContext';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 
 function App() {
   return (
     <>
+      <SpeedInsights />
 
       <ParallaxProvider>
         <CursorProvider>
