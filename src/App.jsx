b21f45efa4ef1d382c6c1ed9b74ab3ce066078ad
Izhann <izhan124@gmail.com>
2024-07-05 18:38:59 +0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,56 @@
 import { ParallaxProvider } from 'react-scroll-parallax'
 import './App.css'
 import Home from './Pages/Home'
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useRef, useState } from 'react';
 import { motion } from 'framer-motion';
-import { CursorProvider } from './context/CursorContext';
+import { CursorContext, CursorProvider } from './context/CursorContext';
 import { SpeedInsights } from '@vercel/speed-insights/react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Custom404 from './Components/Custom404';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
 
 
 function App() {
+  const cursorRef = useRef(null);
+  const mousePosition = useRef({ x: 0, y: 0 });
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      mousePosition.current = { x: e.clientX, y: e.clientY };
+    };
+
+    window.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+  useEffect(() => {
+    const updateCursor = () => {
+      const { x, y } = mousePosition.current;
+      if (cursorRef.current) {
+        cursorRef.current.style.transform = `translate3d(${x - 16}px, ${y - 16}px, 0)`;
+        Object.assign(cursorRef.current.style);
+      }
+      requestAnimationFrame(updateCursor);
+    };
+    updateCursor();
+  }, []);
   return (
     <>
-      <SpeedInsights />
-
-      <ParallaxProvider>
-        <CursorProvider>
-          <Home />
-        </CursorProvider>
-      </ParallaxProvider>
+      <Router>
+        <SpeedInsights />
+        <ParallaxProvider>
+          <CursorProvider>
+            <Header />
+            <Routes>
+              <Route index element={<Home />} />
+              <Route path='*' Component={Custom404} />
+            </Routes>
+            <Footer />
+          </CursorProvider>
+        </ParallaxProvider>
+      </Router >
     </>
   )
 }
--- src/Components/Custom404.jsx
+++ src/Components/Custom404.jsx
@@ -1,19 +1,11 @@
-import React, { useEffect, useRef, useContext, useState } from 'react';
-import MainPage from '../Components/MainPage';
-import About from '../Components/About';
-import Header from '../Components/Header';
-import Footer from '../Components/Footer';
-import Projects from '../Components/Projects';
-import Preloader from '../Components/Preloader';
-import Services from '../Components/Services';
-import { motion } from 'framer-motion';
-import { CursorProvider, CursorContext } from '../context/CursorContext';
-import OurProcess from '../Components/OurProcess';
-import Contact from '../Components/Contact';
+import React, { useContext, useEffect, useRef, useState } from 'react'
+import Marquee from "react-fast-marquee";
+import { MdArrowOutward } from "react-icons/md";
 import ReactGA from 'react-ga4';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { CursorContext, CursorProvider } from '../context/CursorContext';
+import { motion } from 'framer-motion';
 
-const Home = () => {
+const Custom404 = () => {
   const cursorRef = useRef(null);
   const mousePosition = useRef({ x: 0, y: 0 });
   const servicesRef = useRef(null);
@@ -24,8 +16,8 @@ const Home = () => {
   useEffect(() => {
     ReactGA.send({
       hitType: "pageView",
-      page: '/',
-      title: "Home"
+      page: '/*',
+      title: "404"
     })
     const handleMouseMove = (e) => {
       mousePosition.current = { x: e.clientX, y: e.clientY };
@@ -37,7 +29,6 @@ const Home = () => {
       window.removeEventListener('mousemove', handleMouseMove);
     };
   }, []);
-
   useEffect(() => {
     const updateCursor = () => {
       const { x, y } = mousePosition.current;
@@ -50,10 +41,7 @@ const Home = () => {
 
     updateCursor();
   }, [cursorStyle]);
-
-  return (<>
-
-    <Preloader />
+  return (
     <CursorProvider>
       <motion.div
         ref={cursorRef}
@@ -67,20 +55,19 @@ const Home = () => {
           ...cursorStyle,
         }}
       />
-      <main>
-        <Header />
-        <MainPage />
-        <About />
-        <Projects />
-        <Services />
-        <OurProcess />
-        <Contact />
-        <Footer />
-
-      </main>
+      <section className='h-screen flex flex-col w-full bg-black text-white pl-20 items-center justify-center space-y-5'>
+        <div className='w-2/3'>
+          <Marquee loop={0} speed={100} autoFill={true} className='overflow-hidden py-0 lg:py-2 border-t-2 border-b-2'>
+            <p className='text-4xl lg:text-6xl font-black p-3 uppercase tracking-tighter'>404 Error&nbsp;.&nbsp; where am i ?</p>
+          </Marquee>
+        </div>
+        <div className='bg-white text-black pr-5 pl-5 py-2 rounded-full flex items-center justify-evenly w-max group space-x-2 transition-all duration-500'>
+          <div className='bg-black text-white rounded-full hover:scale-100 transition-all duration-500  p-1 '><MdArrowOutward size={20} className='hidden group-hover:block transition-all duration-500' /></div>
+          <p className='font-semibold'>Take me home</p>
+        </div>
+      </section>
     </CursorProvider>
-  </>
-  );
-};
+  )
+}
 
-export default Home;
+export default Custom404
