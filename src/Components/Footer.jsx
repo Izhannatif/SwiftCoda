ef766be9cc4b441f6910fa5348aa0b4b53159384
Izhann <izhan124@gmail.com>
2024-07-06 08:35:27 +0500

    commit

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,9 +4,10 @@ import { FaTwitter, FaInstagram, FaLinkedin } from 'react-icons/fa';
 const Footer = () => {
   const today = new Date()
   return (
-    <footer className="bg-white text-[#1f1f1f] py-3">
+    <footer className="bg-white text-[#1f1f1f] py-2 text-sm">
       <div className="container mx-auto text-center font-normal">
-        <p>© {today.getFullYear()} SwiftCoda. All rights reserved. <br /> Powered By <a target='_blank' href='https://www.encoremnd.com'><span className='underline font-medium'>Encore Marketing and Development</span></a></p>
+        <p>© {today.getFullYear()} SwiftCoda. All rights reserved. </p>
+        <p className='text-sm'> Powered By <a target='_blank' href='https://www.encoremnd.com'><span className='underline font-normal'>Encore Marketing and Development</span></a></p>
       </div>
     </footer>
   );
