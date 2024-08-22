d5d0231fe9af9fbc4a3e61ef7f07090f21818f06
Izhann <izhan124@gmail.com>
2024-08-22 16:54:31 +0500

    commit

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <footer className="bg-white text-[#1f1f1f] py-2 text-sm">
       <div className="container mx-auto text-center font-normal">
         <p>© {today.getFullYear()} SwiftCoda. All rights reserved. </p>
-        <p className='text-sm'> Powered By <a target='_blank' href='https://www.encoremnd.com'><span className='underline font-normal'>Encore Marketing and Development</span></a></p>
+        {/* <p className='text-sm'> Powered By <a target='_blank' href='https://www.encoremnd.com'><span className='underline font-normal'>Encore Marketing and Development</span></a></p> */}
       </div>
     </footer>
   );
